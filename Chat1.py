49e1198af191b5a705b5c4ebe97716be4df7a874
Imanolasolo <jjusturi@gmail.com>
2024-04-19 11:47:49 -0500

    first commit

---
+++ Chat1.py
@@ -0,0 +1,40 @@
+from openai import OpenAI
+import streamlit as st
+from image_loader import render_image
+
+col1, col2 = st.columns([1, 4])
+
+with col1:
+    render_image("logo.png")
+
+with col2:    
+    st.title("ChatGPT with CodeCodix")
+
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        )
+        response = st.write_stream(stream)
+    st.session_state.messages.append({"role": "assistant", "content": response})
